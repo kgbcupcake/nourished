9fb5c9cb180378506d746f9c85dc0d9d886d1567
Maire <kgbcupcake@gmail.com>
2026-09-23 08:50:39 -0400

    feat: Add JEI as a compile-only dependency and enhance Getting Started category with parallax background layers

--- src/main/java/dev/maire/nourished/datagen/book/gettingstarted/GettingStartedCategory.java
+++ src/main/java/dev/maire/nourished/datagen/book/gettingstarted/GettingStartedCategory.java
@@ -4,6 +4,8 @@ import com.klikli_dev.modonomicon.api.datagen.CategoryProvider;
 import com.klikli_dev.modonomicon.api.datagen.SingleBookSubProvider;
 import com.klikli_dev.modonomicon.api.datagen.book.BookCategoryModel;
 import com.klikli_dev.modonomicon.api.datagen.book.BookIconModel;
+import com.klikli_dev.modonomicon.book.BookCategoryBackgroundParallaxLayer;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Items;
 
 public class GettingStartedCategory extends CategoryProvider {
@@ -46,6 +48,16 @@ public class GettingStartedCategory extends CategoryProvider {
         var foodTooltips = this.add(new FoodTooltipsEntry(this).generate('t')).withParent(effects);
         var notifications = this.add(new NotificationsEntry(this).generate('n')).withParent(foodTooltips);
         this.add(new FirstDayEntry(this).generate('y')).withParent(notifications);
+    }
+
+    @Override
+    protected BookCategoryModel additionalSetup(BookCategoryModel category) {
+        // Modonomicon's own nebula/starfield background, layered for parallax scrolling.
+        return category.withBackgroundParallaxLayers(
+                new BookCategoryBackgroundParallaxLayer(ResourceLocation.parse("modonomicon:textures/gui/parallax/flow/base.png"), 0.7f, -1),
+                new BookCategoryBackgroundParallaxLayer(ResourceLocation.parse("modonomicon:textures/gui/parallax/flow/1.png"), 1f, -1),
+                new BookCategoryBackgroundParallaxLayer(ResourceLocation.parse("modonomicon:textures/gui/parallax/flow/2.png"), 1.4f, -1)
+        );
     }
 
     @Override
